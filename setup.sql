764312e8fbcca975fa68c8b814bd28bf13cece62
Matteo Gaudio <matteog1999@gmail.com>
2020-10-11 14:26:11 -0400

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -2,6 +2,8 @@ CREATE DATABASE passwords DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 
 USE passwords;
 
+\! echo 'Creating Tables...';
+
 CREATE TABLE users (
   user_id int NOT NULL PRIMARY KEY,
   first_name varchar(25) NOT NULL,
@@ -24,3 +26,39 @@ CREATE TABLE accounts (
   comment text,
   stamp datetime DEFAULT CURRENT_TIMESTAMP
 );
+
+\! echo 'DONE!';
+
+\! echo 'INSERTING DATA...';
+
+INSERT INTO users VALUES (1, 'Joe', 'Schmoe', 'jschmoe@hotmail.com');
+INSERT INTO users VALUES (2, 'Danny', 'Steele', 'steelydan@gmail.com');
+INSERT INTO users VALUEs (3, 'Randy', 'Savage', 'themachoman@wwe.net');
+
+INSERT INTO websites VALUES (1, 'https://www.amazon.com', 'Amazon');
+INSERT INTO websites VALUES (2, 'http://www.catblog.com', 'Cat Blogs');
+INSERT INTO websites VALUES (3, 'https://www.safebank.com', 'Safe Bank Online Banking');
+INSERT INTO websites VALUES (4, 'http://www.netflix.com', "Netflix");
+
+INSERT INTO accounts (account_id, user_id, website_id, username, password_hash,comment)
+    VALUES (1, 1, 1, 'jobro123', 'moneymachine!', 'Amazon Account');
+INSERT INTO accounts (account_id, user_id, website_id, username, password_hash,comment)
+    VALUES (2, 1, 2, 'katluverjoe', 'bigscarytiger!', 'Joe\'s Cat Blog');
+INSERT INTO accounts (account_id, user_id, website_id, username, password_hash,comment)
+    VALUES (3, 1, 4, 'jostar1997', 'moviechill!', 'Family Netflix Account');
+INSERT INTO accounts (account_id, user_id, website_id, username, password_hash,comment)
+    VALUES (4, 2, 1, 'danofsteel', 'sasha_10112002', 'Danny Personal Amazon Acct');
+INSERT INTO accounts (account_id, user_id, website_id, username, password_hash,comment)
+    VALUES (5, 2, 1, 'DanielSteele', 'workinman1969', 'Dan\'s Business Amazon Acct');
+INSERT INTO accounts (account_id, user_id, website_id, username, password_hash,comment)
+    VALUES (6, 2, 3, 'DSTeele', 'supersafepassword', 'Banking Information');
+INSERT INTO accounts (account_id, user_id, website_id, username, password_hash,comment)
+    VALUES (7, 3, 1, 'MrMachoMan', 'peopleselbow', 'Amazon Account');
+INSERT INTO accounts (account_id, user_id, website_id, username, password_hash,comment)
+    VALUES (8, 3, 2, 'SavageLion', 'slimjim321', 'Macho Man Cat Blog');
+INSERT INTO accounts (account_id, user_id, website_id, username, password_hash,comment)
+    VALUES (9, 3, 3, 'RSavage84', 'petunia2007', 'Randy\'s Bank Account');
+INSERT INTO accounts (account_id, user_id, website_id, username, password_hash,comment)
+    VALUES (10, 3, 4, 'RSavage', 'machomovies', 'Netflix Account');
+
+\! echo 'DONE!';
