a7088019dc2eefd75447b80cc87512621695610e
Amanda Liang <aliang@hartford.edu>
2020-10-12 00:23:35 -0400

    Create "user" table

--- setup.sql
+++ setup.sql
@@ -4,3 +4,11 @@ CREATE DATABASE passwords DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
 
 USE passwords;
 
+/* create tables */
+
+CREATE TABLE user (
+  user_id    SMALLINT(5) NOT NULL,
+  firstname VARCHAR(60) NOT NULL,
+  lastname  VARCHAR(60) NOT NULL,
+  PRIMARY KEY (user_id)
+);
