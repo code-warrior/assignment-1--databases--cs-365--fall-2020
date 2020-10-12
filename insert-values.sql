954a9ca22bfe8326f9ed3b5d0a23f289afb2c6fe
Keith Eagleson <keagleson28@gmail.com>
2020-10-12 19:26:42 -0400

    Changed encryption code

--- insert-values.sql
+++ insert-values.sql
@@ -9,13 +9,17 @@ INSERT INTO website VALUES (8, "https://facebook.com/", "Facebook");
 INSERT INTO website VALUES (9, "https://twitter.com/", "Twitter");
 INSERT INTO website VALUES (10, "https://www.nytimes.com/", "New York Times");
 
-INSERT INTO user VALUES (1, 1, "keith-E", "Keith", "Eagleson", "keagleson28@gmail.com", AES_ENCRYPT("123456", "secretpass"), "Main GitHub account.");
-INSERT INTO user VALUES (2, 1, "keagleson", "Keith", "Eagleson", "keagleson@hartford.edu", AES_ENCRYPT("12345678", "secretpass"), "UHart BlackBoard account.");
-INSERT INTO user VALUES (3, 1, "h4x0r", "Keith", "Eagleson", "obscureemail@gmail.com", AES_ENCRYPT("4!2#qwerTo$2", "secretpass"), "Practice PenTesting.");
-INSERT INTO user VALUES (4, 1, "admin", "Keith", "Eagleson", "keagleson28@gmail.com", AES_ENCRYPT("1234566", "secretpass"), "Admin account for personal/portfolio website.");
-INSERT INTO user VALUES (5, 1, "EagleTV", "Keith", "Eagleson", "eagletv@gmail.com", AES_ENCRYPT("1234567", "secretpass"), "YouTube channel.");
-INSERT INTO user VALUES (6, 1, "keithBuysStuff", "Keith", "Eagleson", "keagleson28@gmail.com", AES_ENCRYPT("asdf123", "secretpass"), "eBay - Buy things I don't need.");
-INSERT INTO user VALUES (7, 1, "SuperCoolUsername", "Anon", "Ymous", "fakeemail@hotmail.com", AES_ENCRYPT("123456asdf", "secretpass"), "Look at cat pictures.");
-INSERT INTO user VALUES (8, 1, "keagleson28@gmail.com", "Keith", "Eagleson", "keagleson28@gmail.com", AES_ENCRYPT("1a3b4c5d6", "secretpass"), "Delete Facebook.");
-INSERT INTO user VALUES (9, 1, "itskeithyy", "Keith", "Eagleson", "keagleson28@gmail.com", AES_ENCRYPT("twitt123", "secretpass"), "Twitter account.");
-INSERT INTO user VALUES (10, 1, "keagleson28@gmail.com", "Keith", "Eagleson", "keagleson28@gmail.com", AES_ENCRYPT("1234", "secretpass"), "NYTimes sub.");
+SET block_encryption_mode = 'aes-256-cbc';
+SET @key_str = SHA2('secretpass', 512);
+SET @init_vector = RANDOM_BYTES(16);
+
+INSERT INTO user VALUES (1, 1, "keith-E", "Keith", "Eagleson", "keagleson28@gmail.com", AES_ENCRYPT("123456", @key_str, @init_vector), "Main GitHub account.");
+INSERT INTO user VALUES (2, 1, "keagleson", "Keith", "Eagleson", "keagleson@hartford.edu", AES_ENCRYPT("12345678", @key_str, @init_vector), "UHart BlackBoard account.");
+INSERT INTO user VALUES (3, 1, "h4x0r", "Keith", "Eagleson", "obscureemail@gmail.com", AES_ENCRYPT("4!2#qwerTo$2", @key_str, @init_vector), "Practice PenTesting.");
+INSERT INTO user VALUES (4, 1, "admin", "Keith", "Eagleson", "keagleson28@gmail.com", AES_ENCRYPT("1234566", @key_str, @init_vector), "Admin account for personal/portfolio website.");
+INSERT INTO user VALUES (5, 1, "EagleTV", "Keith", "Eagleson", "eagletv@gmail.com", AES_ENCRYPT("1234567", @key_str, @init_vector), "YouTube channel.");
+INSERT INTO user VALUES (6, 1, "keithBuysStuff", "Keith", "Eagleson", "keagleson28@gmail.com", AES_ENCRYPT("asdf123", @key_str, @init_vector), "eBay - Buy things I don't need.");
+INSERT INTO user VALUES (7, 1, "SuperCoolUsername", "Anon", "Ymous", "fakeemail@hotmail.com", AES_ENCRYPT("123456asdf", @key_str, @init_vector), "Look at cat pictures.");
+INSERT INTO user VALUES (8, 1, "keagleson28@gmail.com", "Keith", "Eagleson", "keagleson28@gmail.com", AES_ENCRYPT("1a3b4c5d6", @key_str, @init_vector), "Delete Facebook.");
+INSERT INTO user VALUES (9, 1, "itskeithyy", "Keith", "Eagleson", "keagleson28@gmail.com", AES_ENCRYPT("twitt123", @key_str, @init_vector), "Twitter account.");
+INSERT INTO user VALUES (10, 1, "keagleson28@gmail.com", "Keith", "Eagleson", "keagleson28@gmail.com", AES_ENCRYPT("1234", @key_str, @init_vector), "NYTimes sub.");
