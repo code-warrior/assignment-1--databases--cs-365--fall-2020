076172e58bce1cea3729a7e395169b253e63c161
Amanda Liang <aliang@hartford.edu>
2020-10-12 00:24:33 -0400

    create "account" table

--- setup.sql
+++ setup.sql
@@ -20,3 +20,15 @@ CREATE TABLE site (
   PRIMARY KEY (site_id)
 );
 
+CREATE TABLE account (
+  user_id    SMALLINT(5) NOT NULL,
+  site_id    SMALLINT(5) NOT NULL,
+  account_id SMALLINT(5) NOT NULL,
+  username   VARCHAR(60) NOT NULL,
+  password   VARCHAR(60) NOT NULL,
+  email      VARCHAR(60) NOT NULL,
+  time       TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP(),
+  comment      VARCHAR(200),
+  PRIMARY KEY (user_id, site_id, account_id)
+);
+
