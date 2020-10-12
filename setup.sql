d03b2ee7b01b3e5156115e5a0a1f42af01ba1381
Brandon Spada <brandonsgh123127@gmail.com>
2020-10-12 16:08:53 -0400

    Add new file to initialize 10 values into db

--- setup.sql
+++ setup.sql
@@ -1,2 +1,4 @@
 CREATE SCHEMA IF NOT EXISTS `credential_manager` DEFAULT CHARACTER SET utf8;
-USE `credential_manager` ;
+USE `credential_manager`;
+SOURCE gen_tables.sql;
+SOURCE init_table_vals.sql
