d3ef7f2895961f383ba83a31e93c94fdb89c2423
Keith Eagleson <keagleson28@gmail.com>
2020-10-12 13:02:02 -0400

    Added website_id field and changed to primary key

--- commands.sql
+++ commands.sql
@@ -1,5 +1,6 @@
 CREATE TABLE website (
+    website_id SMALLINT(65535) NOT NULL,
     website_url VARCHAR(2083) NOT NULL,
     website_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (website_url)
-)
+    PRIMARY KEY (website_id)
+);
