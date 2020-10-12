7e0200b249d5114ecfba676c984f1a9dd3caa02c
Keith Eagleson <keagleson28@gmail.com>
2020-10-12 13:40:20 -0400

    Added table for users

--- commands.sql
+++ commands.sql
@@ -1,6 +1,15 @@
 CREATE TABLE website (
-    website_id SMALLINT(65535) NOT NULL,
+    website_id SMALLINT(500) NOT NULL,
     website_url VARCHAR(2083) NOT NULL,
     website_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (website_id)
+);
+CREATE TABLE user (
+    website_id SMALLINT(500) NOT NULL,
+    user_id SMALLINT(1000) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (website_id, user_id)
 );
