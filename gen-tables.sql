6b421cf031dd5117cc41564126282782956031c6
Brandon Spada <brandonsgh123127@gmail.com>
2020-10-12 16:03:00 -0400

    Add structure for tables to gen_tables.sql

---
+++ gen-tables.sql
@@ -0,0 +1,20 @@
+use 'credential_manager';
+CREATE TABLE IF NOT EXISTS  account(
+   uid INT(7) NOT NULL,
+   fname VARCHAR(15) NOT NULL,
+   lname VARCHAR(15) NOT NULL,
+   PRIMARY KEY(uid)
+  )    CHARACTER SET 'utf8' COLLATE utf8_general_ci;
+
+  CREATE TABLE IF NOT EXISTS  account_info(
+     uid INT(7) NOT NULL,
+     account_instance_id INT(7) NOT NULL,
+     email VARCHAR(256),
+     username VARCHAR(256),
+     pass VARBINARY(128) NOT NULL, #PRODUCE Binary string values after AES_ENCRYPT
+     site VARCHAR(64) NOT NULL,
+     sitename VARCHAR(128) NOT NULL,
+     timestamp DATETIME  DEFAULT CURRENT_TIMESTAMP,
+     comment VARCHAR(64) NOT NULL,
+     PRIMARY KEY(account_instance_id)
+    )    CHARACTER SET 'utf8' COLLATE utf8_general_ci;
---
+++ setup.sql
@@ -0,0 +1,2 @@
+CREATE SCHEMA IF NOT EXISTS `credential_manager` DEFAULT CHARACTER SET utf8;
+USE `credential_manager` ;
