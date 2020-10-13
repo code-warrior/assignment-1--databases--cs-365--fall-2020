d096212ab761504648fae07b3af5a283d638802b
Amanda Liang <aliang@hartford.edu>
2020-10-13 00:09:12 -0400

    create command to add entry to database

---
+++ commands.sql
@@ -0,0 +1,3 @@
+/* Create a new entry into the database */
+INSERT into account VALUES(3, 2, 1,"TheHermioneGranger",AES_ENCRYPT("allBusiness","rowling"),"hermione_granger@gmail.com",CURRENT_TIMESTAMP(),"");
+
