98d80ee2eb0ca9ecf99ca42381c272e317f2871b
TimothyKodz <55055311+TimothyKodz@users.noreply.github.com>
2020-10-12 22:13:20 -0400

    Update commands for new database structure

--- commands.sql
+++ commands.sql
@@ -1,12 +1,8 @@
 \d //   
 
-INSERT INTO website VALUES (10, "Amazon", "https://www.amazon.com"); 
-    INSERT INTO comment VALUES (10, 0, "Amazon comment"); 
-    INSERT INTO user VALUES (10, 0, "TimAmazon", "TimEmail123@email.com", "Timothy", "Kodz"); 
-    INSERT INTO pass VALUES (10, 0, AES_ENCRYPT('Testpass10', UNHEX(SHA2('Password Encoder', 512))), "14/03/2017"); //
+INSERT INTO account VALUES (10, "Amazon", "https://www.amazon.com", "Amazon comment", "TimAmazon", "TimEmail123@email.com", "Timothy", "Kodz", AES_ENCRYPT('Testpass10', UNHEX(SHA2('Password Encoder', 512))), "14/03/2017") //
 
-SET @web_id = (SELECT website_id FROM website WHERE website_url="https://www.linkedin.com/"); 
-    SET @pass_encrypt = (SELECT password_encrypt FROM pass WHERE website_id = @web_id); 
+SET @pass_encrypt = (SELECT password_encrypt FROM account WHERE website_url LIKE "https://www.tumblr.com/"); 
     SET @pass_decrypt = (AES_DECRYPT(@pass_encrypt, UNHEX(SHA2('Password Encoder', 512)))); 
     SELECT @pass_decrypt//
 
